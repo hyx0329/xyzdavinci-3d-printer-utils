c40b4e914b8fc142eb9c2d372dc6ec0dadfcbb63
hyx0329 <hyx0329@163.com>
2022-10-26 16:02:33 +0800

    faster checksum
    Signed-off-by: hyx0329 <hyx0329@163.com>

--- davinci_printer_utils/gcode_utils.py
+++ davinci_printer_utils/gcode_utils.py
@@ -1,10 +1,14 @@
 # -*- encoding: utf-8 -*-
 
 def gen_checksum(data: [bytes, bytearray]):
-    return sum(data) & 0xFFFFFFFF
+    # faster than sum(), avoid big number
+    ans = 0
+    for i in data:
+        ans = ( ans + i ) & 0xFFFFFFFF
+    return ans
 
 def gen_checksum_bytes(data: [bytes, bytearray]):
-    value = sum(data) & 0xFFFFFFFF
+    value = gen_checksum(data)
     chk = bytes([ (value >> (i * 8)) & 0xFF for i in range(3,-1,-1) ])
     return chk
 
